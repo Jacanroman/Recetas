ca6fd9df5091c28b28d859080b2064645b177ff0
Jacanroman <jacanroman@gmail.com>
2021-01-17 19:15:20 +0000

    adding Trix Editor

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -47,7 +47,9 @@ class RecetaController extends Controller
     {
         $data = request()->validate([
             'titulo' => 'required|min:6',
-            'categoria' => 'required'
+            'categoria' => 'required',
+            'preparacion' => 'required',
+            'ingredientes' => 'required'
         ]);
 
         DB::table('recetas')->insert([
--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" />
+@endsection
 
 @section('botones')
 
@@ -49,12 +52,38 @@
                     @enderror
                 </div>
 
+                <div class="form-group mt-4">
+                    <label for='preparacion'>Preparacion</label>
+                    <input id='preparacion' type='hidden' name="preparacion" value='{{old('preparacion')}}'/>
+                    <trix-editor input='preparacion'></trix-editor>
+                
+                    @error('preparacion')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group mt-4">
+                    <label for='ingredientes'>Ingredientes</label>
+                    <input id='ingredientes' type='hidden' name="ingredientes" value='{{old('ingredientes')}}'/>
+                    <trix-editor input='ingredientes'></trix-editor>
+                    
+                    @error('ingredientes')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Agregar Receta" />
                 </div>
             </form>
         </div>
 
-   
+@endsection
 
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous"></script>
 @endsection
