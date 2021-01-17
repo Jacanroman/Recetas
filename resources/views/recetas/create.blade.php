c4510dbb4dd9d90f0d2a25a0746e641e4bcd375d
Jacanroman <jacanroman@gmail.com>
2021-01-17 00:30:32 +0000

    validation

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -6,12 +6,31 @@
     <a href="{{route('recetas.index')}}" class="btn btn-primary mr-2">Inicio</a>
 
 @endsection
-{{--esto de @section('content') indica que vamos a hacer 
-a tener un bloque de codigo que vamos a meter dentro de content que 
-esta dentro de layouts.app/app.blade.php @yeald('section')--}}
+
 @section('content')
 
-    <h2 class="text-center mb-5">Crear nueva  recetas</h2>
+    <h2 class="text-center mb-5">Crear Nueva Receta</h2>
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form method="POST" action="{{route('recetas.store')}}">
+                @csrf
+                <div class="form-group">
+                    <label for="titulo">Titulo Receta</label>
+                    <input type="text" name="titulo" class="form-control @error('titulo') is-invalid @enderror" id="titulo" placeholder="Titulo Receta" value="{{old('titulo')}}" />
+                
+                    @error('titulo')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta" />
+                </div>
+            </form>
+        </div>
 
    
 
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/recetas', 'RecetaController');
 
 Route::get('/recetas','RecetaController@index')->name('recetas.index');
 Route::get('/recetas/create', 'RecetaController@create')->name('recetas.create');
+Route::post('/recetas', 'RecetaController@store')->name('recetas.store');
 
 Auth::routes();
 
