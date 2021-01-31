9e9f3a1d52a2de28c4bc893f9b04b294a6a57d8b
Jacanroman <jacanroman@gmail.com>
2021-01-31 19:07:12 +0000

    Showing info of the recepies

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -5,7 +5,29 @@
 @endsection
 
 @section('content')
-    <h2 class="titulo-categoria text-uppercase mt-5 mb-4">Ultimas Recetas</h2>
+    
+    <div class="container nuevas-recetas">
+        <h2 class="titulo-categoria text-uppercase mt-5 mb-4">Ultimas Recetas</h2>
 
-    {{$nuevas}}
+        <div class="">
+            @foreach($nuevas as $nueva)
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="/recetaslaravel/storage/app/public/{{$nueva->imagen}}" class="card-img-top" alt="imagen receta">
+
+                    <div class="card-body">
+                        <h3>{{Str::title($nueva->titulo)}}</h3>
+                        {{--para escapar el codigo html se usa !!
+                            o si lo quieres eliminar usar strip_tags()--}}
+                        <p>{{Str::limit(strip_tags($nueva->preparacion), 50)}}</p>
+
+                        <a href="{{route('recetas.show', ['receta' =>$nueva->id])}}"
+                            class="btn btn-primary d-block font-weight-bold text-uppercase"
+                        >Ver Receta </a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
