5bfed9c448cf02f97081abe8ea5cfc837958a960
Jacanroman <jacanroman@gmail.com>
2021-01-31 18:46:16 +0000

    geting the last recepies

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Receta;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -9,7 +10,12 @@ class InicioController extends Controller
     //
     public function index()
     {
-        //vista
-        return view('inicio.index');
+        //Obtener las recetas mas nuevas (las 2 ultimas que se lo decimos con limits)
+        $nuevas = Receta::orderBy('created_at', 'ASC')->limit(2)->get();
+        /*Esto anterior se puede hacer tambien asi
+        $nuevas = Receta::latest()->get(); */
+
+        //vista con compact le pasamos la variable $nuevas
+        return view('inicio.index', compact('nuevas'));
     }
 }
